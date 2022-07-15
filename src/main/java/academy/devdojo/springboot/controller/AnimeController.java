63c93953f044ad49cf0ad97f5a6c44914640eeca
Marcos Alexandria <marcos.alexandria01@gmail.com>
2022-07-15 17:15:29 -0300

    método-get-pt2

--- src/main/java/academy/devdojo/springboot/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot/controller/AnimeController.java
@@ -5,7 +5,9 @@ import academy.devdojo.springboot.service.AnimeService;
 import academy.devdojo.springboot.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +28,10 @@ public class AnimeController {
     public List<Anime> list(){
         log.info(dateUtil.formatDateTimeToDatabaseStyly(LocalDateTime.now()));  //para ver no meu horario o log
         return animeService.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable long id){
+        return ResponseEntity.ok(animeService.findById(id));
     }
 }
--- src/main/java/academy/devdojo/springboot/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot/service/AnimeService.java
@@ -1,12 +1,22 @@
 package academy.devdojo.springboot.service;
 
 import academy.devdojo.springboot.domain.Anime;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 @Service
 public class AnimeService {
+    List<Anime> list = List.of(new Anime(1L,"Naruto"), new Anime(2L,"DBZ"));
     public List<Anime> findAll(){
-        return List.of(new Anime(1L,"Naruto"), new Anime(2L,"DBZ"));
+        return list;
+    }
+
+    public Anime findById(long id){
+        return list.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not fund"));
     }
 }
