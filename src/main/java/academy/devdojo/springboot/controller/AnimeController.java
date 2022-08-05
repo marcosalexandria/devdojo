9a230dd0c6b702042dc26f26018c055fdc874f55
Marcos Alexandria <marcos.alexandria01@gmail.com>
2022-08-05 09:48:58 -0300

    aula 10 método post

--- src/main/java/academy/devdojo/springboot/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot/controller/AnimeController.java
@@ -5,11 +5,9 @@ import academy.devdojo.springboot.service.AnimeService;
 import academy.devdojo.springboot.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -25,13 +23,18 @@ public class AnimeController {
 
 
     @GetMapping //para mapear métodos
-    public List<Anime> list(){
+    public ResponseEntity<List<Anime>> list(){
         log.info(dateUtil.formatDateTimeToDatabaseStyly(LocalDateTime.now()));  //para ver no meu horario o log
-        return animeService.findAll();
+        return new ResponseEntity<>(animeService.findAll(), HttpStatus.OK);
     }
 
     @GetMapping(path = "/{id}")
     public ResponseEntity<Anime> findById(@PathVariable long id){
         return ResponseEntity.ok(animeService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Anime> save(@RequestBody Anime anime){
+        return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
     }
 }
--- src/main/java/academy/devdojo/springboot/service/AnimeService.java
+++ src/main/java/academy/devdojo/springboot/service/AnimeService.java
@@ -5,18 +5,31 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
 @Service
 public class AnimeService {
-    List<Anime> list = List.of(new Anime(1L,"Naruto"), new Anime(2L,"DBZ"));
+    private static List<Anime> animes;
+
+    static {
+        animes = new ArrayList<>(List.of(new Anime(1L,"Naruto"), new Anime(2L,"DBZ")));
+    }
     public List<Anime> findAll(){
-        return list;
+        return animes;
     }
 
     public Anime findById(long id){
-        return list.stream()
+        return animes.stream()
                 .filter(anime -> anime.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not fund"));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3,10000));
+        animes.add(anime);
+        return anime;
     }
 }
